e0696714b2cac0a081f72e184577f249f3c2d3b8
aerrol <aerrol@gmail.com>
2021-05-16 11:19:17 +0800

    added sonarqube-scanner

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -14,7 +14,7 @@ const httpOptions = {
 })
 export class TaskService {
 
-  private apiUrl = 'http://localhost:5000/tasks';
+  private apiUrl = 'http://localhost:3000/tasks';
 
   constructor(private http:HttpClient) { }
 
